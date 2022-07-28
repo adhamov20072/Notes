2cecd90e776aab17b6ed85f6a8157834757da461
Alimardon <adhamov20072@gmail.com>
2022-07-28 18:05:49 +0500

    commit

--- app/src/main/java/com/alimardon/notes/addFragment.kt
+++ app/src/main/java/com/alimardon/notes/addFragment.kt
@@ -5,12 +5,48 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.fragment.findNavController
+import androidx.navigation.fragment.navArgs
+import com.alimardon.notes.Activity.recyclerAdapter
+import com.alimardon.notes.Activity.room.Note
+import com.alimardon.notes.Activity.room.NoteDataBase
+import com.alimardon.notes.databinding.FragmentAddBinding
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.launch
+
 class addFragment : Fragment() {
+    val args: addFragmentArgs by navArgs()
+    lateinit var binding: FragmentAddBinding
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_add, container, false)
+        binding = FragmentAddBinding.inflate(layoutInflater)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        if (args.note == !null) {
+            binding.btn.setText("Update")
+            binding.ed1.setText(args.note!!.title)
+            binding.ed2.setText(args.note!!.description)
+        }
+        binding.btn.setOnClickListener {
+            if (args.note !== null) {
+                val note =
+                    Note(args.note!!.id, binding.ed1.text.toString(), binding.ed2.text.toString())
+                GlobalScope.launch(Dispatchers.IO) {
+                    NoteDataBase.DatabaseBuilder.getdatabase(requireContext()).noteDao()
+                        .updateNote(note)
+                }
+            } else {
+                val note = Note(0, binding.ed1.text.toString(), binding.ed2.text.toString())
+                NoteDataBase.DatabaseBuilder.getdatabase(requireContext()).noteDao()
+                    .insertNote(note)
+            }
+        }
+            findNavController().popBackStack(R.id.addFragment,true)
     }
 }
--- app/src/main/java/com/alimardon/notes/viewFragment.kt
+++ app/src/main/java/com/alimardon/notes/viewFragment.kt
@@ -5,14 +5,56 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-
+import androidx.navigation.fragment.findNavController
+import com.alimardon.notes.Activity.recyclerAdapter
+import com.alimardon.notes.Activity.room.Note
+import com.alimardon.notes.Activity.room.NoteDataBase
+import com.alimardon.notes.databinding.FragmentViewBinding
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.launch
 
 class viewFragment : Fragment() {
+    lateinit var binding: FragmentViewBinding
+    lateinit var adapter: recyclerAdapter
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_view, container, false)
+        binding = FragmentViewBinding.inflate(layoutInflater)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        adapter = recyclerAdapter()
+        adapter.setOnItemClickListener(object : recyclerAdapter.OnItemClickListener {
+            override fun delete(note: Note) {
+                GlobalScope.launch(Dispatchers.IO) {
+                    delete(note)
+                }
+            }
+
+            override fun update(note: Note) {
+                val action = viewFragmentDirections.action_viewFragment_to_addFragment(note)
+                findNavController().navigate(action)
+            }
+        })
+        GlobalScope.launch(Dispatchers.IO) { setlist()}
+        binding.recyclerview.adapter = adapter
+        binding.btnplus.setOnClickListener {
+            findNavController().navigate(R.id.action_viewFragment_to_addFragment)
+
+        }
+    }
+
+    fun setlist() {
+        val notes =
+            NoteDataBase.DatabaseBuilder.getdatabase(requireContext()).noteDao().getAllNotes()
+        adapter.submitList(notes)
+    }
+
+    suspend fun deleteNote(note: Note) {
+        NoteDataBase.DatabaseBuilder.getdatabase(requireContext()).noteDao().deleteNote(note)
     }
 }
