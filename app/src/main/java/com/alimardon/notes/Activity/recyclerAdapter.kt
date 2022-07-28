f505eaf1d3565e032a4ace7a932fd18816e3e1fd
Alimardon <adhamov20072@gmail.com>
2022-07-28 09:10:58 +0500

    first commit

--- app/src/main/java/com/alimardon/notes/Activity/recyclerAdapter.kt
+++ app/src/main/java/com/alimardon/notes/Activity/recyclerAdapter.kt
@@ -1,15 +1,19 @@
 package com.alimardon.notes.Activity
 
+import android.view.LayoutInflater
+import android.view.ViewGroup
 import android.widget.ListAdapter
 import androidx.recyclerview.widget.DiffUtil
 import androidx.recyclerview.widget.RecyclerView
 import com.alimardon.notes.Activity.room.Note
 import com.alimardon.notes.databinding.RecyclerviewLayoutBinding
 
-class recyclerAdapter : ListAdapter<Note, recyclerAdapter.MyViewHolder>(diffUtil) {
+class recyclerAdapter :
+    androidx.recyclerview.widget.ListAdapter<Note, recyclerAdapter.MyViewHolder>(diffUtil) {
     class MyViewHolder(val binding: RecyclerviewLayoutBinding) :
         RecyclerView.ViewHolder(binding.root)
 
+    private var listner: OnItemClickListener? = null
 
     companion object {
         val diffUtil = object : DiffUtil.ItemCallback<Note>() {
@@ -21,5 +25,30 @@ class recyclerAdapter : ListAdapter<Note, recyclerAdapter.MyViewHolder>(diffUtil
                 return oldItem.id == newItem.id
             }
         }
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MyViewHolder {
+        return MyViewHolder(
+            RecyclerviewLayoutBinding.inflate(
+                LayoutInflater.from(parent.context),
+                parent,
+                false
+            )
+        )
+    }
+
+    override fun onBindViewHolder(holder: MyViewHolder, position: Int) {
+        val item = getItem(position)
+        holder.binding.title.text=item.title
+        holder.binding.description.text=item.description
+    }
+
+    interface OnItemClickListener {
+        fun delete(note: Note)
+        fun update(note: Note)
+    }
+
+    fun setOnItemClickListener(onItemClickListener: OnItemClickListener) {
+        listner = onItemClickListener
     }
 }
